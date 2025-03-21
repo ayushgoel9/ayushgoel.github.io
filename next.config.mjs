56d052f53a138fed522b70a4618507dcd21bcf40
Ayush Goel <ayushgoe@buffalo.edu>
2025-03-20 21:07:32 -0400

    Update Next.js config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,14 @@ try {
 const nextConfig = {
   output: 'export',
   basePath: '',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
   experimental: {
     webpackBuildWorker: true,
